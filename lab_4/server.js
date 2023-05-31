b975a632982e5dbef1a0d60c57427e959e2e9881
Ju1ce13 <ruslan3346@gmail.com>
2023-05-31 13:17:36 +0300

    'laba9'

--- lab_4/server.js
+++ lab_4/server.js
@@ -1,59 +1,76 @@
-var express = require("express"),
-http = require("http"),
-app = express(),
-toDos =
-[
-  {
-    "description": "Посмотреть список групп ИКТЗИ",
-    "tags": [ "список", "ИКТЗИ" ]
-  },
+const express = require("express");
+const http = require("http"); 
+const mongoose = require("mongoose");
+const ToDosController = require("./controller/ToDosController.js");
+const UsersController = require("./controller/UserController.js");
 
-  {
-    "description": "Посмотреть список групп ИАНТЭ",
-    "tags": [ "список", "ИАНТЭ" ]
-  },
+let app = express();
 
-  {
-    "description": "Посмотреть список групп ИАЭП",
-    "tags": [ "список", "ИАЭП" ]
-  },
+http.createServer(app).listen(3000); // Начинаем слушать запросы
 
-  {
-    "description": "Посмотреть список групп ИРЭФ - ЦТ",
-    "tags": [ "список", "ИРЭФ - ЦТ" ]
-  },
+app.use('/', express.static(__dirname + '/client'));
+app.use('/user/:username', express.static(__dirname + '/client'));
 
-  {
-    "description": "Посмотреть список групп ФМФ",
-    "tags": [ "список", "ФМФ" ]
-  },
-
-  {
-    "description": "Посмотреть список групп ВШПИТ",
-    "tags": [ "список", "ВШПИТ" ]
-  },
-
-  {
-    "description": "Посмотреть список групп ИИЭиП",
-    "tags": [ "список", "ИИЭиП" ]
-  }
-]
-
-app.use(express.static(__dirname + "/client"));
-http.createServer(app).listen(3000);
-
-app.get("/todos.json", function (req, res) {
-	res.json(toDos);
-});
-
-app.use(express.static(__dirname + "/client"));
-
+// командуем Express принять поступающие объекты JSON
 app.use(express.urlencoded({ extended: true }));
-app.post("/todos", function (req, res) { // сейчас объект сохраняется в req.body
-	var newToDo = req.body;
-	console.log(newToDo);
-	toDos.push(newToDo);
 
-	res.json({"message":"Вы размещаетесь на сервере!"}); // отправляем простой объект
+mongoose.connect('mongodb://0.0.0.0:27017/tvshow', { useNewUrlParser: true, useUnifiedTopology: true })  // Подключаемся к БД
+	.then(() => { // Успешное подключение
+        	console.log('db connected...');
+    	})
+    	.catch(() => { // Подключение безуспешно
+        	console.log('bad connection...');
+    	});
+
+app.get("/todos.json", ToDosController.index);
+app.get("/todos/:id", ToDosController.show); 
+app.post("/todos", ToDosController.create);
+app.put("/todos/:id", ToDosController.update);
+app.delete("/todos/:id", ToDosController.destroy);
+
+app.get("/users/:username/todos.json", ToDosController.index);
+app.post("/users/:username/todos", ToDosController.create);
+app.put("/users/:username/todos/:id", ToDosController.update);
+app.delete("/users/:username/todos/:id", ToDosController.destroy);
+
+app.get("/users.json", UsersController.index); 
+app.post("/users", UsersController.create); 
+app.get("/users/:username", UsersController.show);
+app.put("/users/:username", UsersController.update);
+app.delete("/users/:username", UsersController.destroy);
+
+/* --------------------------------------------------------------------------------------------------
+
+let ToDo = mongoose.model('ToDo', new Schema({ description: String, tags: [ String ] })); // Создаем модель данных
+
+app.get("/todos.json", async (req, res) => { // Настраиваем маршрутизатор для GET-запроса
+		await ToDo.find() // Выбираем все объекты модели данных
+				.then((toDos) => { // Успешная читка
+					res.json(toDos);
+				})
+				.catch((err) => { // Ошибка читки
+					console.log(err);
+				});
 });
 
+app.post("/todos", async (req, res) => { // Настроиваем маршрутизатор для POST-запроса
+	console.log(req.body);
+	let newToDo = new ToDo({"description":req.body.description, "tags":req.body.tags});
+	
+	const newToDosDoc = await newToDo.save() // Сохраняем (добавляем) новые данные в модель данных
+			.then(async (err, result) => { // Данные успешно сохранены
+				await ToDo.find()
+					.then(async (err, result) => { // Успешная читка
+						res.json(result);
+					})
+					.catch(async (err, result) => { // Ошибка читки
+						res.send('ERROR');
+					});
+			})
+			.catch(async (err, result) => { // Ошибка сохранения
+				console.log(err);
+				res.send('ERROR');
+			});
+});
+
+-------------------------------------------------------------------------------------------------- */
